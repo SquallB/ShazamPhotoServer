e206f3662be9a6b586cb8b6474050155eee316c9
SquallB <mazento2002@yahoo.fr>
2015-02-07 23:10:31 +0100

    added monumentDAO

--- class/MonumentDAO.class.php
+++ class/MonumentDAO.class.php
@@ -8,28 +8,36 @@ class MonumentDAO extends DAO {
 		parent::__constrct($connection);
 	}
 
-	private function queryToMonument($result) {
-
-	}
-
 	public function find($id) {
-		 $request = $this->getConnection()->prepare('
-			SELECT *
-			FROM monument
-			WHERE id = :id
+		 $stmt = $this->getConnection()->prepare('
+			SELECT m.id, m.photoPath, m.year, m.nbVisitors, m.nbLikes, l.id as l_id, l.latitude, l.longitude, a.id as a_id, a.number, a.street, ci.id as ci_id, ci.name as ci_name, co.id as co_id, co.name as co_name
+			FROM monument m
+			INNER JOIN localization l ON m.localization_id = l.id
+			INNER JOIN address a ON m.address_id = a.id
+			INNER JOIN city ci ON a.city_id = ci.id
+			INNER JOIN country co ON ci.country_id = co.id
+			WHERE m.id = :id
 		');
-		$request->bindParam(':id', $id);
-		$request->execute();
-		return $this->queryToMonument($request->fetch());
+		$stmt->bindParam(':id', $id);
+		$stmt->execute();
+		return new Monument($stmt->fetch());
 	}
 
 	public function findAll() {
-		$request = $this->getConnection()->prepare('
-			SELECT *
-			FROM monument
+		$stmt = $this->getConnection()->prepare('
+			SELECT m.id as m_id, m.photoPath, m.year, m.nbVisitors, m.nbLikes, l.id as l_id, l.latitude, l.longitude, a.id as a_id, a.number, a.street, ci.id as ci_id, ci.name as ci_name, co.id as co_id, co.name as co_name
+			FROM monument m
+			INNER JOIN localization l ON m.localization_id = l.id
+			INNER JOIN address a ON m.address_id = a.id
+			INNER JOIN city ci ON a.city_id = ci.id
+			INNER JOIN country co ON ci.country_id = co.id
 		');
-		$request->execute();
-		return $request->fetchAll();
+		$stmt->execute();
+		$array = array();
+		foreach($stmt->fetchAll() as $row) {
+			$array[] = new Monument($row);
+		}
+		return $array;
 	}
 
 	public function save($data) {
@@ -37,6 +45,10 @@ class MonumentDAO extends DAO {
 	}
 
 	public function update($data) {
+
+	}
+
+	public function delete($data) {
 
 	}
 }
